34eb8c24bdc20600a0839f9332e1f58ac20718ed
ViperIrs <115434504+ViperIrs@users.noreply.github.com>
2024-02-28 21:14:17 -0300

    Delete Lista 2 directory

--- Lista 2/exercicio3.py
+++
@@ -1,8 +0,0 @@
-def contar_palavras(frase):
-    palavras = frase.split()  
-    return len(palavras)
-
-frase_exemplo = input("Digite uma frase:")
-num_palavras = contar_palavras(frase_exemplo)
-
-print(f"A frase '{frase_exemplo}' tem {num_palavras} palavras.")
